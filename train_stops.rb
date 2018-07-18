64361104a607f19007eb85d35daf474de8997852
tannyque <tannyque1992@gmail.com>
2018-07-18 22:36:40 +0100

    Deletes Cumbernauld from the array by index

--- train_stops.rb
+++ train_stops.rb
@@ -9,15 +9,12 @@ stops.insert(4, "Polmont")
 stops.index("Linlithgow")
 # Remove "Livingston" from the array using its name
 stops.delete("Livingston")
+# Delete "Cumbernauld" from the array by index
+stops.delete_at(2)
 p stops
 
-# Complete these tasks:
-# Add "Edinburgh Waverley" to the end of the array
-# Add "Glasgow Queen St" to the start of the array
-# Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
-# Work out the index position of "Linlithgow"
-# Remove "Livingston" from the array using its name
-# Delete "Cumbernauld" from the array by index
+
+
 # How many stops there are in the array?
 # How many ways can we return "Falkirk High" from the array?
 # Reverse the positions of the stops in the array
