3e03879c6e9528e807ddf77af91018b3ac6d7398
tannyque <tannyque1992@gmail.com>
2018-07-18 22:39:39 +0100

    Counts how many stops there are in the array

--- train_stops.rb
+++ train_stops.rb
@@ -11,11 +11,10 @@ stops.index("Linlithgow")
 stops.delete("Livingston")
 # Delete "Cumbernauld" from the array by index
 stops.delete_at(2)
+# How many stops there are in the array?
+a = stops.count()
 p stops
 
-
-
-# How many stops there are in the array?
 # How many ways can we return "Falkirk High" from the array?
 # Reverse the positions of the stops in the array
 # Print out all the stops using a for loop
