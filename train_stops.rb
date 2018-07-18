6c8564adadc02dd81b2e6b2f0f446e65921adfe6
tannyque <tannyque1992@gmail.com>
2018-07-18 22:04:13 +0100

    Initial Commit

---
+++ train_stops.rb
@@ -0,0 +1,18 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# Add "Edinburgh Waverley" to the end of the array
+stops.push('Edinburgh Waverly')
+# Add "Glasgow Queen St" to the start of the array
+stops.unshift('Glasgow Queen St')
+p stops
+
+# Complete these tasks:
+# Add "Edinburgh Waverley" to the end of the array
+# Add "Glasgow Queen St" to the start of the array
+# Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+# Work out the index position of "Linlithgow"
+# Remove "Livingston" from the array using its name
+# Delete "Cumbernauld" from the array by index
+# How many stops there are in the array?
+# How many ways can we return "Falkirk High" from the array?
+# Reverse the positions of the stops in the array
+# Print out all the stops using a for loop
