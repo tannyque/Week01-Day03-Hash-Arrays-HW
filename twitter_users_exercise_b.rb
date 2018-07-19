3a9b4d85c707e9fc645d2ced35063a5cf7496c7f
tannyque <tannyque1992@gmail.com>
2018-07-19 08:11:00 +0100

    Gets Erik's lottery numbers

--- twitter_users_exercise_b.rb
+++ twitter_users_exercise_b.rb
@@ -63,6 +63,9 @@ users['Jonathan'][:twitter]
 users['Erik'][:home_town]
 
 # Get the array of Erik's lottery numbers
+
+users['Erik'][:lottery_numbers]
+
 # Get the type of Avril's pet Monty
 # Get the smallest of Erik's lottery numbers
 # Return an array of Avril's lottery numbers that are even
