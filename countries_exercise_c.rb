b4fdd2ec895050d7e8108ee69121d11dd911968a
tannyque <tannyque1992@gmail.com>
2018-07-19 10:25:20 +0100

    Prints names of countries and finds total population

--- countries_exercise_c.rb
+++ countries_exercise_c.rb
@@ -18,9 +18,41 @@ united_kingdom = [
 
 # Change the capital of Wales from "Swansea" to "Cardiff".
 
-united_kingdom [0][:capital] = "Cardiff"
+united_kingdom [1][:capital] = "Cardiff"
 
 
 # Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+
+# united_kingdom = [
+#   {
+#   name: "Northern Ireland"
+#   population: 181000
+#   capital: "Belfast"
+# },
+# ]
+
+norther_ireland = {
+  name: "Northern Ireland",
+  population: 181000,
+  capital: "Belfast"
+}
+united_kingdom.push(norther_ireland)
+
+
+p united_kingdom
+
 # Use a loop to print the names of all the countries in the UK.
+
+for country in united_kingdom
+  p country[:name]
+end
+
 # Use a loop to find the total population of the UK.
+
+total = 0
+
+for country in united_kingdom
+  total += country[:population]
+end
+
+p total
